709d04266649d597a4e18235c14372cb7147f47d
Hrishikesh <hkrai39@gmail.com>
2022-03-05 23:22:46 +0530

    disable landscape

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Ticket from "./Components/Ticket";
 function App() {
   return (
     <div className="root" style={{ backgroundImage: `url('/bg.png')` }}>
-      <img className="prizes" src="/top-header.png" alt="" />
+      <img className="top-header" src="/top-header.png" alt="" />
       <div className="App">
         <div className="wrapper">
-          <img className="prizes" src="/tambola.png" alt="" />
+          <img className="tambola" src="/tambola.png" alt="" />
           <Ticket />
           <img className="prizes" src="/option-tray.png" alt="" />
         </div>
